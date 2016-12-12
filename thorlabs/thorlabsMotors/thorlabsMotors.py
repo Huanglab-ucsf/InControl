5035104826fad49b3fbe543278aebc27573282b7
huanglab-ucsf <huanglabucsf@gmail.com>
2016-12-12 15:54:15 -0800

    1212_Byers-3

--- thorlabs/thorlabsMotors/thorlabsMotors.py
+++ thorlabs/thorlabsMotors/thorlabsMotors.py
@@ -7,9 +7,10 @@ import time
 class Control(inLib.Device):
     def __init__(self, settings):
         inLib.Device.__init__(self, 'thorlabs.thorlabsMotors.thorlabsMotors_api', settings)
+        self.position = 0.
 
     def initServo(self):
-         self._api.initServo()
+        self._api.initServo()
 
     def jogUp(self):
         self._api.jogUp()
@@ -28,6 +29,7 @@ class Control(inLib.Device):
         This should be tested for the real step size
         '''
         dpos = int(pos*10)
+        print(dpos)
         self._api.setStepsize(dpos)
 
     def shutDown(self):
