595ed5ed9a5fc4830654c8e5f5863c1225b7545c
huanglab-ucsf <huanglabucsf@gmail.com>
2016-12-16 13:14:56 -0800

    1216_Byers-8

--- modules/BMC_multicorrection/BMC_multicorrection_design.py
+++ modules/BMC_multicorrection/BMC_multicorrection_design.py
@@ -7,7 +7,7 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
-fromt matplotlibwidget import MatplotlibWidget
+from matplotlibwidget import MatplotlibWidget
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
