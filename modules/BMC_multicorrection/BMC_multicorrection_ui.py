c71abde94d6e8f7114d58e69f7cb5d4e4c2378ee
Huanglab-ucsf <huanglabucsf@gmail.com>
2017-03-09 15:59:53 -0800

    0309_Byers-2

--- modules/BMC_multicorrection/BMC_multicorrection_ui.py
+++ modules/BMC_multicorrection/BMC_multicorrection_ui.py
@@ -180,7 +180,7 @@ class UI(inLib.ModuleUI):
         act_ind = self._switch_zern()
         start_coeffs = self.z_comps.get_parameters(act_ind)[0] # only get those
         flabel = self._ui.lineEdit_evname.text() #
-        Nmeasure = self.spinbox_evsteps.value()
+        Nmeasure = self._ui.spinBox_evsteps.value()
         self.EV_thread = Optimize_pupil(self.Evolution, act_ind, start_coeffs, Nmeasure, flabel)
         self.EV_thread.finished.connect(self._evolution_ready)
         self._ui.pushButton_evolve.setEnabled(False)
