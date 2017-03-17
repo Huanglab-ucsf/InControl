196c1311978daf43747c06a37659c65f2eaf564d
huanglab-ucsf <huanglabucsf@gmail.com>
2017-03-17 15:57:29 -0700

    0317_Byers-2

--- thorlabs/thMotor_dll/thMotor_dll_api.py
+++ thorlabs/thMotor_dll/thMotor_dll_api.py
@@ -1,3 +1,12 @@
+'''
+APT motor controller for Thorlabs, to be integrated into the package InControl
+Core functions adapted from https://github.com/mcleung/PyAPT/blob/master/PyAPT.py
+
+Dan Xie
+dan.xie@ucsf.edu
+'''
+
+
 import os
 from ctypes import c_long, c_buffer, c_float, windll, pointer
 print os.getcwd()
@@ -17,11 +26,136 @@ class API():
             self.aptdll.EnableEventDlg(True)
             self.aptdll.APTInit()
             self.HWtype = c_long(HWtype)
+            self.SerialNum = SerialNum
+            self.init_hardware() # initialization
 
+
+
+
+    def init_hardware(self):
+        '''
+        Initialises the motor.
+        '''
+        if self.verbose:
+            print 'initializeHardwareDevice serial', self.SerialNum
+
+        result = self.aptdll.InitHWDevice(self.SerialNum)
+        if result == 0:
+            self.connected = True
+            if self.verbose:
+                print 'initialization SUCESS'
+            return True
+        else:
+            raise Exception('Connection Failed. Check Serial Number!')
+            return False
+
+    def get_stageInfo(self):
+        '''
+        Get the stage information
+        '''
+        min_pos = c_float()
+        max_pos = c_float()
+        units = c_long()
+        pitch = c_float()
+        self.aptdll.MOT_GetStageAxisInfo(self.SerialNum, pointer(min_pos), pointer(max_pos), pointer(units), pointer(pitch))
+        stageInfo = [min_pos.value, max_pos.value, units.value, pitch.value]
+        return stageInfo
+
+
+    def set_stageInfo(self):
+        '''
+        Set the stage information
+        '''
+        min_pos = c_float(min_pos)
+        max_pos = c_fload(max_pos)
+        units = c_long(1)
+        pitch = c_float(self.config.get_pitch())
+        self.aptdll.MOT_SetStageAxisInfo(self.SerialNum, min_pos, max_pos, units, pitch)
+        return True
+
+    def get_pos(self):
+        '''
+        get current position.
+        '''
+        if self.verbose:
+            print("get_pos probing...")
+        if not self.connected:
+            raise Exception('Device not connected! ')
+
+        position = c_float()
+        self.aptdll.MOT_GetPosition(self.SerialNum, pointer(position))
+        if self.verbose:
+            print("Current position:", position.value)
+        return position.value
+
+    def move_by(self, rpos):
+        '''
+        Move by rpos from current position.
+        '''
+        if self.verbose:
+            print("Move by:", rpos, c_float(rpos))
+        if not self.connected:
+            raise Exception('Please connect first!')
+
+        cr_pos = c_float(rpos)
+        self.aptdll.MOT_MoveRelativeEx(self.SerialNum, cr_pos, True)
+        if self.verbose:
+            print("Moved successfully by", rpos)
+        return True
+
+
+    def move_to(self, apos):
+        '''
+        move the motor to the position pos.
+        '''
+        if self.verbose:
+            print("Move to", apos, c_float(apos))
+        if not self.connected:
+            raise Exception('Please connect first!')
+
+        ca_pos = c_float(apos)
+        self.aptdll.MOT_MoveAbsoluteEx(self.SerialNum, ca_pos, True)
+        if self.verbose:
+            print("Moved successfully to", apos)
+        return True
+
+
+    def jog_up(self):
+        '''
+        just jog the stage up, assume the stage is connected.
+        '''
+        self.aptdll.MOT_MoveJog(self.SerialNum, 1, True)
+
+    def jog_down(self):
+        '''
+        jog the stage dowm, assume the stage is connected.
+        '''
+        self.aptdll.MOT_MoveJog(self.SerialNum, 2, True)
+
+    def identify(self):
+        '''
+        I don't quite understand the purpose of this function, but I copied it here anyway.
+        '''
+        self.aptdll.MOT_Identify(self.SerialNum)
+        return True
+
+    def clean_up(self):
+        self.aptdll.APTCleanUp()
+        if self.verbose:
+            print('APT cleaned up')
+            self.connected = False
 
 
 def main():
-    API_test = API()
+    API_test = API(verbose = True)
+    if API_test.connected:
+        API_test.identify()
+        API_test.jog_up()
+        API_test.jod_down()
+        API_test.clean_up()
+    else:
+        print("Initialization fails.")
+
 
 if __name__ == "__main__":
     main()
